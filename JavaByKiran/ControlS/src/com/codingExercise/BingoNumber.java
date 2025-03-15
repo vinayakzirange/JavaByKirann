d323a33e0b42429adf3d2d8983c9040f296699c1
vinayakzirange <vinayakzirange@gmail.com>
2025-03-15 14:08:12 +0530

    Update BingoNumber.java

--- JavaByKiran/ControlS/src/com/codingExercise/BingoNumber.java
+++ JavaByKiran/ControlS/src/com/codingExercise/BingoNumber.java
@@ -4,20 +4,24 @@ public class BingoNumber {
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 	        System.out.print("Enter a number: ");
-	        int number = scanner.nextInt();
-  if (isDivisibleBySeven(number) && !containsSeven(number)) {
-	            System.out.println("Bingo Number");
-	        } else {
-	            System.out.println("Not A Bingo Number");
-	        }
-	        scanner.close();
-	    }
-	    private static boolean isDivisibleBySeven(int num) {
-	        return num % 7 == 0;
-	    }
-	    private static boolean containsSeven(int num) {
-	        String numStr = Integer.toString(Math.abs(num));
-	        return numStr.contains("7");
+	     int number = scanner.nextInt();
+        boolean isDivisible = number % 7 == 0;
+        boolean containsSeven = false;
+        int temp = Math.abs(number); 
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digit == 7) {
+                containsSeven = true;
+                break;
+            }
+            temp = temp / 10;
+        }
+        if (isDivisible && !containsSeven) {
+            System.out.println("Bingo Number");
+        } else {
+            System.out.println("Not A Bingo Number");
+        }
+        scanner.close();
 	    }
 	}
 
